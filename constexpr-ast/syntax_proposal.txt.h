85b60ccfe048831f5f61351628021b7268201a25
Nemeth Peter <hun.nemethpeter@gmail.com>
2015-04-19 15:36:10 +0200

    Updated syntax proposal with <for> .. </for> idea

--- constexpr-ast/syntax_proposal.txt.h
+++ constexpr-ast/syntax_proposal.txt.h
@@ -31,25 +31,23 @@ class ClassNameAsMember
 template<typename T>
 class MirrorClass()
 {
-  for <member : T.class.members()>
-    // syntax 1
+  <for> (member : T.class.members())
+    // syntax 1 - normal declaration syntax, but type come from member.type()
+    //            id come from member.name()
     ''member.type()'' ''member.name()'';
 
     // syntax 2
     ''member.name()'': member.type();
-  for;
 
-  // A Preliminary Proposal for a Static if
-  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3322.pdf
-  //
-  // static if declaration
-  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3329.pdf
-  //
-  // “Static If” Considered
-  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3613.pdf
-  if <member.size() == 0>
-    char dummy[4];
-  if;
+    // syntax 3 - type first then name
+    ''member.type()'': member.name();
+
+    // syntax 4 - xml like syntax
+    <auto type="member.type()" name="member.name()"/>
+
+    // syntax 4 - cast to a decltype
+    (decltype)member.type(): member.name();
+  </for>
 }
 
 Example
@@ -75,3 +73,49 @@ std::ast::ast_identifier template_id = "test";
 ''template_id''<int>;
 result: test<int>;
 
+// ========== if<> ============
+template<typename T>
+struct TestClass()
+{
+  // A Preliminary Proposal for a Static if
+  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3322.pdf
+  //
+  // static if declaration
+  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3329.pdf
+  //
+  // “Static If” Considered
+  // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3613.pdf
+  //
+  // 1. statif if was two keyword
+  //
+  // static
+  //
+  // /*
+  //   blah blah blah
+  // */
+  //
+  // if (sizeof(T)==8) {
+  //   gun(32);
+  // else
+  //   gun();
+  // }
+  //
+  // 2. static if braces looks like a scope
+  //
+  // 3. If we do not provide a static_for, hackers will simply make one out of static_if or fake equivalents.
+  //
+  //
+  // now:
+  // - <if> and </if> is one keyword, no space allowed
+  // - <else> also different
+  // - <if> doesn't use braces
+  // - <for> is provided
+  // - new, but familiar syntax
+
+  <if> (member.size() == 0)
+    char dummy[4];
+  <else>
+    <if> (...)
+    </if>
+  </if>
+}
