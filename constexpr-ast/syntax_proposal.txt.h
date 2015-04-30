32124801ec2bff13fcaa78d7ed2183d91c9360ef
Nemeth Peter <hun.nemethpeter@gmail.com>
2015-05-01 01:05:34 +0200

    Updated proposals

--- constexpr-ast/syntax_proposal.txt.h
+++ constexpr-ast/syntax_proposal.txt.h
@@ -29,30 +29,38 @@ class ClassNameAsMember
 //    int Player;
 // };
 
+// syntax evolution:
+// =================
+// syntax 1 - normal declaration syntax, but type come from member.type()
+//            id come from member.name()
+''member.type()'' ''member.name()'';
+
+// syntax 2 - name first then type
+''member.name()'': member.type();
+
+// syntax 3 - type first then name
+''member.type()'': member.name();
+
+// syntax 4 - xml like syntax
+<auto type="member.type()" name="member.name()"/>
+
+// syntax 4 - cast to a decltype
+(decltype)member.type(): member.name();
+
+// preprocessor like syntax
+#<type>member.type()#</type> #<id>member.name()#</id>;
+
+// simplified preprocessor like syntax
+#<member.type()> #<member.name()>;
+
+// simplified preprocessor like syntax 2
+#<member.type()/> #<member.name()/>;
+
+
 template<typename T>
 class MirrorClass()
 {
   #<for> (member : T.class.members()>
-    // syntax 1 - normal declaration syntax, but type come from member.type()
-    //            id come from member.name()
-    ''member.type()'' ''member.name()'';
-
-    // syntax 2 - name first then type
-    ''member.name()'': member.type();
-
-    // syntax 3 - type first then name
-    ''member.type()'': member.name();
-
-    // syntax 4 - xml like syntax
-    <auto type="member.type()" name="member.name()"/>
-
-    // syntax 4 - cast to a decltype
-    (decltype)member.type(): member.name();
-
-    // preprocessor like syntax
-    #<type>member.type()#</type> #<id>member.name()#</id>;
-
-    // simplified preprocessor like syntax
     #<member.type()> #<member.name()>;
   #</for>
 
@@ -68,17 +76,17 @@ constexpr std::ast::ast_var ast = a.class;
 result: int a1;
 
 // stringify something
-''"ast.name()"''
+#<stringify>ast.name()</stringify>
 result: "a"
 
 // generic expression call
 std::ast::ast_identifier function_id = "test";
-''function_id''(1, 2, 3);
+#<function_id>(1, 2, 3);
 result: test(1, 2, 3);
 
 // generic template instantiation
 std::ast::ast_identifier template_id = "test";
-''template_id''<int>;
+#<template_id><int>;
 result: test<int>;
 
 // ========== if<> ============
@@ -108,26 +116,17 @@ struct TestClass()
   //   gun();
   // }
   //
-  // 2. static if braces looks like a scope
+  // 2. "[..] static if braces looks like a scope [..]"
   //
-  // 3. If we do not provide a static_for, hackers will simply make one out of static_if or fake equivalents.
+  // 3. "[..] If we do not provide a static_for, hackers will simply make one out of static_if or fake equivalents. [..]"
   //
   //
   // now:
-  // - <if> and </if> is one keyword, no space allowed
-  // - <else> also different
-  // - <if> doesn't use braces
-  // - <for> is provided
+  // - #<if> and #</if> is one keyword, no space allowed
+  // - #<else> also different
+  // - #<if> doesn't use braces
+  // - #<for> is provided
   // - new, but familiar syntax
-
-  <if> (member.size() == 0)
-    char dummy[4];
-  <else>
-    <if> (...)
-    </if>
-  </if>
- or
-
 #<if> (member.size() == 0)
     char dummy[4];
 #<else>
@@ -135,8 +134,9 @@ struct TestClass()
   #</if>
 #</if>
 
-#<id>#</id>
-#<type>#</type>
+#<var.name()>
+#<var.type()>
+
 #<for>#</for>
 
   #<for> (member : T.class.members()>
